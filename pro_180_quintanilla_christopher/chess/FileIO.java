2460eeb99e66a7eee42cc774dc93cc121c12b557
Chris Quintanilla <Ca.q@live.com>
2014-05-12 10:49:36 -0600

    Signed-off-by: Chris Quintanilla <Ca.q@live.com>

--- pro_180_quintanilla_christopher/chess/FileIO.java
+++ pro_180_quintanilla_christopher/chess/FileIO.java
@@ -1,7 +1,20 @@
 package pro_180_quintanilla_christopher.chess;
 
+import java.io.*;
 
 public class FileIO
 {
-
+	public FileIO()
+	{
+		File file = new File("src/ChessMoves.txt");
+		FileInputStream fiStream = null;
+		try
+		{
+			fiStream = new FileInputStream(file);
+		} catch (FileNotFoundException e1)
+		{
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+	}
 }
